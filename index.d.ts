8321fa651162736133244f9811159bb0302f02f7
Federico Giuggioloni <federico.giuggioloni@studenti.unitn.it>
2021-11-19 23:09:42 +0100

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,6 @@
 declare module "moment-with-locales-es6" {
-    import * as moment from "moment/min/moment-with-locales.min.js";
+    import * as moment from "moment/min/moment.min.js";
+    import 'moment/locale/it';
+    moment.locale('it');
     export default moment;
 }
